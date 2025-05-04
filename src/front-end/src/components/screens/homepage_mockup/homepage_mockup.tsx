cf9eb4b2919abcd6af51783d7b04ed5307fbb2ff
Toby <tobycrust@gmail.com>
2025-05-04 21:59:13 +1000

    Updated front end

--- src/front-end/src/components/screens/homepage_mockup/homepage_mockup.tsx
+++ src/front-end/src/components/screens/homepage_mockup/homepage_mockup.tsx
@@ -1,44 +1,32 @@
-import { ShoppingCartIcon } from "lucide-react";
+import { MenuIcon } from "lucide-react";
 import React from "react";
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-} from "../../ui/avatar";
-import { Button } from "../../ui/button";
-import { FooterByAnima } from "./sections/footer/index";
-import { FrameByAnima } from "./sections/frame/index";
+import { Button } from "../../../components/ui/button";
+import { FooterByAnima } from "./sections/footer";
+import { FrameByAnima } from "./sections/frame";
 
-export const HomepageMockUp = (): JSX.Element => {
+export const HomepageMvp = (): JSX.Element => {
     return (
-        <div className="flex flex-col min-w-[992px] max-w-[1658px] items-start gap-10 bg-white">
-            <header className="flex w-full items-center justify-between p-10 bg-[#e0b8f3] shadow-[0px_5px_4px_#00000040]">
+        <div className="flex flex-col w-full max-w-[1658px] items-start gap-10 bg-white">
+            <header className="flex items-center justify-between p-10 w-full bg-[#e0b8f3] shadow-[0px_5px_4px_#00000040]">
                 <img
                     className="w-[255px] h-[34.5px] object-cover"
                     alt="Logo"
-                    src="https://c.animaapp.com/m9ziqhftNQs4Nv/img/image-2-1.png"
+                    src="https://c.animaapp.com/ma9eugodIUDWMx/img/image-2-1.png"
                 />
 
-                <div className="flex items-center justify-end gap-9">
+                <div className="flex items-center gap-8">
                     <Button
                         variant="outline"
-                        className="flex items-center gap-2.5 px-6 py-2.5 bg-[#fffdfd] rounded-[32px] hover:bg-[#f8f0fc]"
+                        className="px-6 py-2.5 bg-[#fffdfd] rounded-[32px] hover:bg-[#f8f0f8]"
                     >
             <span className="[font-family:'Hannari-Regular',Helvetica] font-normal text-[#ae53da] text-base">
-              Account
+              Log in
             </span>
-                        <Avatar className="w-6 h-6">
-                            <AvatarImage
-                                src="https://c.animaapp.com/m9ziqhftNQs4Nv/img/person.svg"
-                                alt="Person"
-                            />
-                            <AvatarFallback>AC</AvatarFallback>
-                        </Avatar>
                     </Button>
 
-                    <Button variant="ghost" size="icon" className="p-0">
-                        <ShoppingCartIcon className="w-7 h-7 text-black" />
-                    </Button>
+                    <div className="flex items-center justify-end">
+                        <MenuIcon className="w-7 h-7 text-black" />
+                    </div>
                 </div>
             </header>
 
