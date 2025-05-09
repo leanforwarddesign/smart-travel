37e99ed7e881ac86f7fc8df88634246993c2cd21
Toby <tobycrust@gmail.com>
2025-05-09 20:39:13 +1000

    Cleanup

--- src/front-end/src/components/screens/index.tsx
+++ src/front-end/src/components/screens/index.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HomepageMockUp } from "./homepage_mockup/homepage_mockup";
+import { HomepageMvp } from "./homepage_mockup/homepage_mockup";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
     <StrictMode>
-        <HomepageMockUp />
+        <HomepageMvp />
     </StrictMode>,
 );
